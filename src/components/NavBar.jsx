3435157218788db32ca5a3044316fdbeb0ca427f
Juan Gilsanz Polo <juangilsanz@gmail.com>
2022-05-05 19:33:41 +0200

    Created navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const NavBar = () => {
+import Icon from '../assets/img/breaking-bad.png';
+
+const NavBar = ({pageTitle}) => {
     return (
         <div className='navbar'>
-
+            <div className='left-content'>
+                <div className='page-title'>{pageTitle}</div>
+            </div>
+            <img className='icon' src={Icon} alt="Breaking Bad icon" />
         </div>
     );
 }
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -4,7 +4,7 @@ import NavBar from '../components/NavBar';
 const Characters = () => {
     return (
         <div className='characters-page'>
-            <NavBar />
+            <NavBar pageTitle="All characters" />
         </div>
     );
 }
