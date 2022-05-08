91f6098fca600df3386e7b363e13af37db2f8839
Juan Gilsanz Polo <juangilsanz@gmail.com>
2022-05-08 20:44:06 +0200

    Added character not found message

--- src/pages/CharacterDetails.jsx
+++ src/pages/CharacterDetails.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { HalfMalf } from 'react-spinner-animated';
 
+import Modal from '../components/Modal';
+
 import useStore from '../store/useStore';
 import QuoteIcon from '../assets/img/quote-icon.svg';
 import { fetchRandomQuote } from '../services/api-requests';
@@ -20,13 +22,19 @@ const CharacterDetails = () => {
 
     const [selectedCharacter, setSelectedCharacter] = useState();
     const [quote, setQuote] = useState("");
+    const [characterNotFound, setCharacterNotFound] = useState(false);
     const [loadingQuote, setLoadingQuote] = useState(true);
 
     const location = useLocation();
 
     useEffect(() => {
         const character = characters.find(character => character.char_id.toString() === location.pathname.substring(1));
-        setSelectedCharacter(character);
+        if (character) {
+            setSelectedCharacter(character);
+        }
+        else {
+            setCharacterNotFound(true);
+        }
     }, [characters, location.pathname]); 
 
     const callFetchQuote = async () => {
@@ -69,90 +77,99 @@ const CharacterDetails = () => {
     }, [selectedCharacter]);
 
     return (
-        <div className='character-details-page'>
-            {
-                !loadingCharacters ? (
-                    selectedCharacter ? (
-                        <div className='character'>
-                            <div className='top'>
-                                <div className='left'>
-                                    <div className='img-div'>
-                                        <img src={selectedCharacter.img} alt="Character" />
+        <>
+            <Modal 
+                open={characterNotFound} 
+                title="Character not found" 
+                content="This character doesn't exist"
+                defaultButtons={true}
+                onClose={() => setCharacterNotFound(false)}
+            />
+            <div className='character-details-page'>
+                {
+                    !loadingCharacters ? (
+                        selectedCharacter ? (
+                            <div className='character'>
+                                <div className='top'>
+                                    <div className='left'>
+                                        <div className='img-div'>
+                                            <img src={selectedCharacter.img} alt="Character" />
+                                        </div>
+                                        <div className='name'>{selectedCharacter.name}</div>
                                     </div>
-                                    <div className='name'>{selectedCharacter.name}</div>
-                                </div>
-                                <div className='right'>
-                                    {
-                                        !loadingQuote ? (
-                                            quote ? (
-                                                <div className='quote-div'>
-                                                    <div className='quote'>
-                                                        <img className='start-quote' src={QuoteIcon} alt="Quote icon" />
-                                                        <img className='end-quote' src={QuoteIcon} alt="Quote icon" />
-                                                        <span className='quote-text'>{quote}</span>
-                                                    </div>  
-                                                    <button className='refresh-quote' onClick={() => callFetchQuote()}>
-                                                        <i className="bi bi-arrow-repeat"></i>
-                                                    </button>
-                                                </div>
+                                    <div className='right'>
+                                        {
+                                            !loadingQuote ? (
+                                                quote ? (
+                                                    <div className='quote-div'>
+                                                        <div className='quote'>
+                                                            <img className='start-quote' src={QuoteIcon} alt="Quote icon" />
+                                                            <img className='end-quote' src={QuoteIcon} alt="Quote icon" />
+                                                            <span className='quote-text'>{quote}</span>
+                                                        </div>  
+                                                        <button className='refresh-quote' onClick={() => callFetchQuote()}>
+                                                            <i className="bi bi-arrow-repeat"></i>
+                                                        </button>
+                                                    </div>
+                                                ) : (
+                                                    <span className='no-quote'>No quote available.</span>
+                                                )
                                             ) : (
-                                                <span className='no-quote'>No quote available.</span>
+                                                <div className='loading-quote'>
+                                                    <HalfMalf center={false} />
+                                                </div>
                                             )
-                                        ) : (
-                                            <div className='loading-quote'>
-                                                <HalfMalf center={false} />
-                                            </div>
-                                        )
-                                    }
+                                        }
+                                    </div>
+                                </div>
+                                <div className='bottom'>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td>Nickname</td>
+                                                <td>{selectedCharacter.nickname}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Portrayed</td>
+                                                <td>{selectedCharacter.portrayed}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Birthday</td>
+                                                <td>{selectedCharacter.birthday}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Occupation</td>
+                                                <td>
+                                                    {
+                                                        selectedCharacter.occupation.map((item, index) => `${item}${index < selectedCharacter.occupation.length-1 ? ', ' : ''}`)
+                                                    }
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Apperance</td>
+                                                <td>
+                                                    {
+                                                        selectedCharacter.appearance.map((item, index) => `${item}${index < selectedCharacter.appearance.length-1 ? ', ' : ''}`)
+                                                    }
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Status</td>
+                                                <td>{selectedCharacter.status}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
-                            <div className='bottom'>
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <td>Nickname</td>
-                                            <td>{selectedCharacter.nickname}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Portrayed</td>
-                                            <td>{selectedCharacter.portrayed}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Birthday</td>
-                                            <td>{selectedCharacter.birthday}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Occupation</td>
-                                            <td>
-                                                {
-                                                    selectedCharacter.occupation.map((item, index) => `${item}${index < selectedCharacter.occupation.length-1 ? ', ' : ''}`)
-                                                }
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Apperance</td>
-                                            <td>
-                                                {
-                                                    selectedCharacter.appearance.map((item, index) => `${item}${index < selectedCharacter.appearance.length-1 ? ', ' : ''}`)
-                                                }
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Status</td>
-                                            <td>{selectedCharacter.status}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                        ) : null
+                    ) : (
+                        <div className='loading'>
+                            <HalfMalf center={false} />
                         </div>
-                    ) : null
-                ) : (
-                    <div className='loading'>
-                        <HalfMalf center={false} />
-                    </div>
-                )
-            }
-        </div>
+                    )
+                }
+            </div>
+        </>
     );
 }
  
